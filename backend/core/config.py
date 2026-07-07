f1130a862588744cb30e91c4684c82283dda6f18
MUIZ UD DIN <mueezgamings@gmail.com>
2026-07-07 20:26:03 +0500

    created backend config

---
+++ backend/core/config.py
@@ -0,0 +1,26 @@
+# Browser Identity
+
+USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+
+VIEWPORT = {
+    "width": 1280,
+    "height": 800
+}
+
+# Timezone
+
+LOCALE="en-US"
+TIMEZONE="America/New_York"
+
+# Operational Modes
+
+HEADLESS=False
+SLOW_MO=50
+
+# Target Location
+
+BASE_URL= "https://www.google.com/maps"
+
+# Security/Timeouts
+
+TIMEOUT=30000
